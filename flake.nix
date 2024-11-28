55030c0cba65ded42908b9c7af5c6ef0c66ffa8a
Ben Suskins <suskinsdevelopment@gmail.com>
2024-11-28 22:35:24 +0000

    Add mkalias for gui apps

--- flake.nix
+++ flake.nix
@@ -9,13 +9,17 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = [ 
           pkgs.neovim
           pkgs.ansible
           pkgs.gh
+          pkgs.mkalias
           pkgs.alacritty
         ];
 
@@ -23,6 +27,27 @@
           pkgs.nerd-fonts.fira-code
           pkgs.nerd-fonts.jetbrains-mono
       ];
+
+      # Create alias' for GUI apps in Applications
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+        '';
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
